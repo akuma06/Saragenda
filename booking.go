7001916c6ec32f3863f7c4cb6163cc09507832f1
akuma <demon.akuma06@gmail.com>
2018-12-03 15:23:44 +0100

    Add user agent for get request

--- booking.go
+++ booking.go
@@ -33,7 +33,11 @@ func (bp *BookingParser) Parse(icalUrl *url.URL) error {
 }
 
 func (bp BookingParser) LoadIcal(icalUrl *url.URL) ([]*ical.Node, error) {
-	resp, err := http.Get(icalUrl.String())
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", icalUrl.String(), nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+
+	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
